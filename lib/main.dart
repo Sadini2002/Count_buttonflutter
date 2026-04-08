29b6a08e882eafccb2766574ae062e4407974702
Sadini <jasingha2002sadninipunika@gamil.com>
2026-04-08 16:03:44 +0530

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -4,80 +4,83 @@ void main() {
   runApp(MyApp());
 }
 
-// Main App
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: CounterPage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
   }
 }
 
-// Stateful Widget
-class CounterPage extends StatefulWidget {
-  @override
-  _CounterPageState createState() => _CounterPageState();
-}
-
-class _CounterPageState extends State<CounterPage> {
-  int count = 0;
-
-  void increment() {
-    setState(() {
-      count++;
-    });
-  }
-
-  void decrement() {
-    setState(() {
-      count--;
-    });
-  }
-
-  void reset() {
-    setState(() {
-      count = 0;
-    });
-  }
+class LoginPage extends StatelessWidget {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Advanced Counter App"), centerTitle: true),
-      body: Container(
-        width: double.infinity,
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Counter Value", style: TextStyle(fontSize: 24)),
-            SizedBox(height: 10),
-            Text(
-              "$count",
-              style: TextStyle(
-                fontSize: 48,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(height: 30),
-
-            // Buttons Row
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      backgroundColor: Colors.blue[50],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(onPressed: increment, child: Text("Increase")),
-                ElevatedButton(onPressed: decrement, child: Text("Decrease")),
+                // Logo / Title
+                Text(
+                  "My Social App",
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+
+                SizedBox(height: 40),
+
+                // Email Field
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+
+                // Password Field
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                  ),
+                ),
+
+                SizedBox(height: 30),
+
+                // Login Button
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      print("Login Clicked");
+                    },
+                    child: Text("Login"),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+
+                // Sign Up Text
+                TextButton(onPressed: () {}, child: Text("Create New Account")),
               ],
             ),
-
-            SizedBox(height: 20),
-
-            ElevatedButton(
-              onPressed: reset,
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-              child: Text("Reset"),
-            ),
-          ],
+          ),
         ),
       ),
     );
