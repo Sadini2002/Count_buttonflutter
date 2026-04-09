be9298ef74c3fdd6915033f9a0b87c218cd9a2b7
Sadini <jasingha2002sadninipunika@gamil.com>
2026-04-09 14:44:43 +0530

    profile

--- lib/main.dart
+++ lib/main.dart
@@ -7,78 +7,121 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
+    return MaterialApp(
+      title: 'Profile App',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
-class LoginPage extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[50],
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Logo / Title
-                Text(
-                  "My Social App",
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.blueAccent,
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blueAccent, Colors.purpleAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Card(
+            elevation: 10,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: EdgeInsets.all(20),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage('assets/profile.jpg'),
                   ),
-                ),
+                  SizedBox(height: 20),
 
-                SizedBox(height: 40),
-
-                // Email Field
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    hintText: "Email",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.email),
+                  Text(
+                    "Sadini Nipunika",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                   ),
-                ),
 
-                SizedBox(height: 20),
+                  SizedBox(height: 5),
 
-                // Password Field
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.lock),
+                  Text(
+                    "Flutter Developer",
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
                   ),
-                ),
 
-                SizedBox(height: 30),
+                  SizedBox(height: 20),
 
-                // Login Button
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "120",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text("Posts"),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "500",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text("Followers"),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "300",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text("Following"),
+                        ],
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(height: 20),
+
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 40,
+                        vertical: 12,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      backgroundColor: Colors.blueAccent,
+                    ),
                     onPressed: () {
-                      print("Login Clicked");
+                      print("Follow Clicked");
                     },
-                    child: Text("Login"),
+                    child: Text("Follow", style: TextStyle(fontSize: 16)),
                   ),
-                ),
-
-                SizedBox(height: 10),
-
-                // Sign Up Text
-                TextButton(onPressed: () {}, child: Text("Create New Account")),
-              ],
+                ],
+              ),
             ),
           ),
         ),
